dbd180a8e66a26a1f72d28a1747da3eda5d3ba39
chrismbryant <cbryant@berkeley.edu>
2019-04-07 17:25:59 -0700

    Create functional magnifying window

--- js/feature_viz.js
+++ js/feature_viz.js
@@ -1,55 +1,69 @@
 var svg = d3.select("#svg_main");
 var svg_grid = d3.select("#svg_grid");
 
+var counter = 0;
+var throttle_mod;
+
 var box_size = 100;
-var box_opacity = 0.1;
+var box_opacity = 0.4;
 var box_stroke_opacity = 0;
 var frozen = false;
-var [x, y] = [0, 0];
+var [box_x, box_y] = [0, 0];
 const default_box_opacity = box_opacity;
 
 const size = 500;
 const width = size;
 const height = size;
 const padding = 0.25;
-const point_radius = 10;
-const point_opacity = 0.8;
+const point_radius = 2;
+const point_opacity = 0.5;
 const ease_duration = 180;
+const cell_size = size;
+var r_scaled = point_radius * cell_size/size;
 
-svg.attr("width", width)
-  .attr("height", height);
+var m = 1;
+var data;
+var data_2d;
+var labels;
+var label_type;
+var data_tags;
+var feature_names;
+
+svg.attr("width", size)
+  .attr("height", size);
 
 svg_grid.attr("width", width)
   .attr("height", height);
 
-const file = "data/test.json";
+const file = "data/random_small.json";
 
 d3.queue()
   .defer(d3.json, file)
   .await(main);
+
+// ----------------------------------------------------------------------------- //
 
 function main(error, my_data) {
 
   if (error) throw error;
   
   const keys = Object.keys(my_data);
-  const data = my_data["data"];
-  const data_2d = transform_data(my_data["data_2d"], width, height, padding);
-  const labels = my_data["labels"];
-  const label_type = my_data["label_type"];
-  const data_tags = my_data["label_tags"];
-  const feature_names = my_data["feature_names"];
 
-  if (data_2d.length != data.length) {
-    console.log("Warning: data sizes don't match");
-  }
+  data = my_data["data"];
+  data_2d = transform_data(my_data["data_2d"], width, height, padding);
+  m = data_2d.length;
+  throttle_mod = d3.max([1, d3.min([Math.round(m/2000), 20])]);
+  console.log(throttle_mod);
+  labels = my_data["labels"];
+  label_type = my_data["label_type"];
+  data_tags = my_data["label_tags"];
+  feature_names = my_data["feature_names"];
 
-  g = plot_data(svg, data_2d, labels);
+  var g_main = plot_data(svg, data_2d, labels);
+  var g_grid = plot_data(svg_grid, data_2d, labels);
+
   init_box(box_size);
   animate_box();
-
-  g_grid = plot_data(svg_grid, data_2d, labels);
-  replot_grid(svg_grid, data_2d);
 
 }
 
@@ -91,14 +105,6 @@ function plot_data(svg, data_2d, labels) {
   return g
 }
 
-function replot_grid() {
-  g_grid.selectAll("point")
-    .attr("cx", function(d){
-      console.log(d);
-      return d * 2;
-    });
-}
-
 function init_box(size) {
   svg.append("rect")
     .attr("id", "ref_box")
@@ -122,17 +128,19 @@ function animate_box() {
     .on("mouseenter", on_mouseenter);
 }
 
-function move_box(pos, size, dur=0) {
-  [x, y] = pos.map(v => v - size/2);
-  x = d3.min([d3.max([x, 0]), width - size]);
-  y = d3.min([d3.max([y, 0]), height - size]);
+function move_box(pos, box_size, dur=0) {
+  [box_x, box_y] = pos.map(v => v - box_size/2);
+  box_x = d3.min([d3.max([box_x, 0]), width - box_size]);
+  box_y = d3.min([d3.max([box_y, 0]), height - box_size]);
   d3.select("#ref_box")
     .transition()
     .ease(d3.easeExp)
     .duration(dur)
-    .attr("x", x)
-    .attr("y", y)
+    .attr("x", box_x)
+    .attr("y", box_y)
     .attr("fill-opacity", box_opacity);
+  counter++
+  position_grid_points(box_x, box_y, box_size, dur);
 }
 
 function resize_box() {
@@ -145,7 +153,7 @@ function resize_box() {
   return box_size;
 }
 
-function change_box_fill(frozen) {
+function change_box_style(frozen) {
   if (frozen == true) {
     box_opacity = 0;
     box_stroke_opacity = 1;
@@ -158,13 +166,31 @@ function change_box_fill(frozen) {
     .attr("stroke-opacity", box_stroke_opacity);
 }
 
-// --------------------------------- CONTROLS --------------------------------- //
+function zoom_grid_points(box_size) {
+  r_scaled = width/box_size * point_radius * cell_size/size;
+  if (counter % throttle_mod != 0 && frozen == false) {return}
+  d3.select("#svg_grid")
+    .selectAll("circle.point")
+    .attr("r", r_scaled);
+}
+
+function position_grid_points(box_x, box_y, box_size, dur=0) {
+  if (counter % throttle_mod != 0 && frozen == false) {return}
+  d3.select("#svg_grid")
+    .selectAll("circle.point")
+    .transition()
+    .ease(d3.easeExp)
+    .duration(dur)
+    .attr("cx", function(d) {return cell_size/box_size * (d[0] - box_x);})
+    .attr("cy", function(d) {return cell_size/box_size * (d[1] - box_y);});
+}
+
+// ------------------------------- MAIN CONTROLS ------------------------------- //
 
 function on_mousemove() {
   if (frozen == false) {
     var pos = d3.mouse(this);
     move_box(pos, box_size);
-    replot_grid();
   }
 }
 
@@ -172,6 +198,7 @@ function on_wheel_zoom() {
   if (frozen == false) {
     var pos = d3.mouse(this);
     box_size = resize_box();
+    zoom_grid_points(box_size);
     move_box(pos, box_size);
   }
 }
@@ -179,13 +206,12 @@ function on_wheel_zoom() {
 function on_click() {
   if (frozen == false) {
     frozen = true;
-    change_box_fill(frozen);
   } else {
     frozen = false;
-    var pos = d3.mouse(this);
-    change_box_fill(frozen);
-    move_box(pos, box_size, ease_duration);
   }
+  var pos = d3.mouse(this);
+  change_box_style(frozen);
+  move_box(pos, box_size, ease_duration);
 }
 
 function on_mouseleave() {
@@ -199,6 +225,7 @@ function on_mouseenter() {
   if (frozen == false) {
     var pos = d3.mouse(this);
     console.log(pos);
+    zoom_grid_points(box_size);
     move_box(pos, box_size, 2 * ease_duration);
   }
 }
